42457084c5dccbb01c361496bd2ce1f328cffd22
jurajperic <juraj.peric@gmail.com>
2022-06-17 13:14:57 +0200

    added slider on info screen

--- app/src/main/java/com/example/findthestatue/InformationActivity.kt
+++ app/src/main/java/com/example/findthestatue/InformationActivity.kt
@@ -1,24 +1,43 @@
 package com.example.findthestatue
 
-
-
-
 import android.net.Uri
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.util.Log
+import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 
 class InformationActivity : AppCompatActivity() {
-
+    private var mBottomSheetBehavior: BottomSheetBehavior<View?>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_information)
         val imageView = findViewById<ImageView>(R.id.photo_holder)
+        val bottomSheet = findViewById<ConstraintLayout>(R.id.bottom_sheet_layout)
+        val description = findViewById<TextView>(R.id.description)
+        val title = findViewById<TextView>(R.id.title)
         val uri = intent.getStringExtra("URI")
         imageView.setImageURI(Uri.parse(uri))
+
+
+        val bottomSheetBehaviour = BottomSheetBehavior.from(bottomSheet).apply {
+            peekHeight = 400
+            this.state = BottomSheetBehavior.STATE_COLLAPSED
+        }
+
+        bottomSheet.setOnClickListener{
+            if(bottomSheetBehaviour.state == BottomSheetBehavior.STATE_COLLAPSED) bottomSheetBehaviour.state = BottomSheetBehavior.STATE_EXPANDED
+
+        }
+
+
+
 
     }
 
